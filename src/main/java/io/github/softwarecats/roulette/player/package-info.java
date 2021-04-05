33483936830a85a108fc2d250491d5c613d0b0b2
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2021-04-05 17:00:29 +1000

    Update copyright

--- src/main/java/io/github/softwarecats/roulette/player/package-info.java
+++ src/main/java/io/github/softwarecats/roulette/player/package-info.java
@@ -14,25 +14,4 @@
  * limitations under the License.
  */
 
-package io.github.softwarecats.roulette;
-
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class BinTest {
-
-    @Test
-    public void testConstructor() {
-        Outcome outcome1 = new Outcome("Name 1", 1);
-        Outcome outcome2 = new Outcome("Name 2", 2);
-        Outcome[] outcomes = new Outcome[]{outcome1, outcome2};
-
-        Bin bin1 = new Bin();
-
-        Bin bin2 = new Bin(outcomes);
-
-        Bin bin3 = new Bin(new ArrayList<>(Arrays.asList(outcomes)));
-    }
-}
+package io.github.softwarecats.roulette.player;
